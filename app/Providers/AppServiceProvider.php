03b3e297862716009cd400df1aac1211ffc9e12d
ObryanMazariegosRK <cmazariegosc5@miumg.edu.gt>
2026-06-25 17:19:44 -0600

    feat: implementacion de CRUD completo de categorias con arquitectura limpia

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,25 @@
 
 namespace App\Providers;
 
+use App\Application\Abstractions\Category\IDeleteCategoryUseCase;
+use App\Application\Abstractions\Category\IGetCategoriesByParentIdUseCase;
 use Illuminate\Support\ServiceProvider;
+
+use App\Application\UseCases\Category\GetCategoriesByParentIdUseCase;
+
+//Importamos las interfaces
+use App\Domain\Abstractions\ICategoryRepository;
+use App\Application\Abstractions\Category\IGetCategoriesUseCase;
+use App\Application\Abstractions\Category\IGetCategoryByIdUseCase;
+use App\Application\Abstractions\Category\ISaveCategoryUseCase;
+use App\Application\Abstractions\Category\IUpdateCategoryUseCase;
+use App\Application\UseCases\Category\DeleteCategoryUseCase;
+//Importamos los casos de uso y los repositorios
+use App\Data\Repositories\CategoryRepository; 
+use App\Application\UseCases\Category\GetCategoriesUseCase;
+use App\Application\UseCases\Category\GetCategoryByIdUseCase;
+use App\Application\UseCases\Category\SaveCategoryUseCase;
+use App\Application\UseCases\Category\UpdateCategoryUseCase;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,11 +29,52 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        /**
+         * $this->App es el objeto global del conedor de servicios
+         * ->bind(..) es el método que crea el mapeo, recibe dos 
+         * parámetros la llave (interfaz) y el valor (clase concreta)
+         * ::class es un atajo para no escribir el namespace completo
+         */
+        $this->app->bind(
+            ICategoryRepository::class,
+            CategoryRepository::class
+        );
+
+     
+        $this->app->bind(
+            IGetCategoriesUseCase::class,
+            GetCategoriesUseCase::class
+        );    
+
+        $this->app->bind(
+        ISaveCategoryUseCase::class,
+        SaveCategoryUseCase::class
+        );
+
+        $this->app->bind(
+        IUpdateCategoryUseCase::class,
+        UpdateCategoryUseCase::class);
+
+        $this->app->bind(
+        IDeleteCategoryUseCase::class, 
+        DeleteCategoryUseCase::class);
+
+        $this->app->bind(
+        IGetCategoryByIdUseCase::class, 
+        GetCategoryByIdUseCase::class);
+
+        $this->app->bind(
+        IGetCategoriesByParentIdUseCase::class, 
+        GetCategoriesByParentIdUseCase::class);
+
     }
 
     /**
      * Bootstrap any application services.
+     * Método que se ejecuta después de que todos los services
+     * providers de la aplicaciónhan sido registrados, se usa para arrancar
+     * herramientas que necesiten que las dependnecias ya estén listas
+     * Actualmente no necesitamos tener alguna configuracion aqui
      */
     public function boot(): void
     {
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,23 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::get('/categories', [CategoryController::class, 'index']);
+
+Route::post('/categories', [CategoryController::class, 'store']);
+
+Route::put('/categories/{id}', [CategoryController::class, 'update']);
+
+Route::get('/categories/parent/{parentId?}', [CategoryController::class, 'getByParent']);
+
+Route::get('/categories/{id}', [CategoryController::class, 'show']);
+
+Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+
